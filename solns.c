f6202f1ea4404bab73e7c609e9eec8388c97ba37
chanverse <68290563+chanverse@users.noreply.github.com>
2021-06-18 20:39:50 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -1,19 +1,6 @@
 /* Enter your solutions in this file */
 #include <stdio.h>
-#include <cs50>
 
-int max(int [], int);
-
-int main(void){
-int t=0;
-   int n=get_int("enter a integer: ");
-   int arr[n]={0};
-   for(int i=0;i<n;i++){
-   arr[i]=get_int();
-   }
-   max(arr[],n];
-   }
-   
  int max(int arr[], int n){  
    for(int j=0;j<n;j++){
    arr[i+1]=t;
