75928a7f9bc0bfa567273da70313db2e7a703a3c
chanverse <68290563+chanverse@users.noreply.github.com>
2021-06-18 20:53:59 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -15,3 +15,73 @@
    }
    return arr[n-1];
 }
+
+int min(int arr[], int n){ 
+  int t=0;
+   for(int i=0;i<n;i++){
+   arr[i+1]=t;
+   if(arr[i]>arr[i+1]){
+      arr[i+1]=arr[i];
+      arr[i]=t;
+      }
+    else{
+     continue;
+     }  
+   }
+   return arr[0];
+}
+
+float average(int arr[], int n)
+{
+    float sum=0;
+    float avg=0
+    for(int i=0;i<n;i++)
+    {
+        sum+=arr[i];
+    }
+    avg=sum/size
+    return (avg);
+}
+
+
+int mode(int num[], int n)
+{
+    int x=0,y=0;
+  for(int i=0;i<n;++i){
+    int z=0;
+    for(int j=0;j<n;++j){
+      if(num[j]==num[i])
+        z++;
+    }
+    if(z>x){
+      x =z;
+      y = num[i];
+    }
+  }
+  return(y);
+}
+
+int factors(int num, int f[])
+{
+    int n=num;
+   
+    int prime[]={2,3,5,7,9,11,13,17,19};
+    int n=(int)(sizeof(prime)/sizeof(int));
+    for( int i=0;n!=1;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            if((n%prime[j])==0)
+            {
+                f[i]=prime[j];
+                n/=prime[j];
+                break;
+            }
+            else
+            {
+                continue;
+            }
+        }
+    }
+    return i;
+}
